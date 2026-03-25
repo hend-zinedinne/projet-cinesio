9cd8bc0b4f3c9f230533b3401886fe990a3d09dd
hend-zinedinne <zinedinnehend@gmail.com>
2026-03-25 14:21:21 +0100

    Implementation de la base de données dans le catalogue

--- src/repositories/filmRepository.php
+++ src/repositories/filmRepository.php
@@ -15,3 +15,25 @@ function getAllFilms(): array
 
     return $films;
 }
+function getPaysAbregeFromFilm($idFilm): int
+{
+    $connexion = getConnexion();
+
+    // Requête paramétrée
+    $requeteSQL = "SELECT initiale
+    FROM film, pays
+    WHERE film.id_pays = pays.id
+    AND nom_etudiant = :id";
+    $requete = $connexion->prepare($requeteSQL);
+    // Execution : donner une valeur au paramètre :nom
+    $requete->bindValue("id", $idFilm);
+    $requete->execute();
+
+    $pays = $requete->fetchAll(PDO::FETCH_ASSOC);
+
+    if (!$pays) {
+        return null;
+    } else {
+        return $pays;
+    }
+}
